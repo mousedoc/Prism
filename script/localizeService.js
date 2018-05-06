df35c767800e02aa14fdecef296d6b8699cea345
MouseDoc <sangyun3631@gmail.com>
2018-05-06 22:49:21 +0900

    add language system

--- script/localizeService.js
+++ script/localizeService.js
@@ -1,20 +1,26 @@
 let languageObject = require("../resource/json/language.json");
-let currentLanguage = require('electron').app.getLocale();
+// let currentLanguage = require('electron').app.getLocale();
 
 function toLocalize(key)
 {
+    var currentLanguage = 'en';
+
     for (var i = 0; i < languageObject.length; i++)
     {
-        if (languageObject[i].id == key)
+        var elem = languageObject[i];
+        console.log(elem);
+        if (elem.id == key)
         {
-            var elem = languageObject[i];
             switch(currentLanguage)
             {
                 case 'ko':
+                    console.log(elem.korean);
                     return elem.korean;
                 case 'ja':
+                    console.log(elem.japanese);
                     return elem.japanese;
                 default:
+                    console.log(elem.english);
                     return elem.english;
             }
         }
@@ -24,6 +30,5 @@ function toLocalize(key)
 module.exports =
 {
     LanguageObject : languageObject,
-    CurrentLanguage : currentLanguage,
     ToLocalize : toLocalize,
 };
